819e050f533d57bf74f56c5aa7571ba8ce786455
Julio César <soul.unleashed13@gmail.com>
2023-11-09 14:21:03 -0600

    Cambiada imagen

--- src/components/moleculas/FavoreceSeccion.tsx
+++ src/components/moleculas/FavoreceSeccion.tsx
@@ -6,9 +6,9 @@ const FavoreceSeccion = () => {
 		<div className="bg-con-primary lg:min-h-[25rem] min-h-[40rem] w-full flex lg:flex-row flex-col lg:py-0 py-10">
 			<div className="lg:w-[40%] relative flex justify-center items-center">
 				<Image
-					src="/assets/03-Asset_11img.png"
+					src="/assets/nueva-img-home.png"
 					alt=""
-					width={450}
+					width={500}
 					height={100}
 					className="absolute lg:-bottom-14 -bottom-[40rem]"
 				/>
