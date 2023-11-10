16395da52c06a294b87265b9f3799f14c56325d7
Julio César <soul.unleashed13@gmail.com>
2023-11-10 12:02:08 -0600

    Se cambio imagen en seccion favorecer

--- src/components/moleculas/FavoreceSeccion.tsx
+++ src/components/moleculas/FavoreceSeccion.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const FavoreceSeccion = () => {
 
 	return (
-		<div className="bg-[url('/assets/1Asset_24foto-BIEN.jpg')] bg-center bg-cover lg:min-h-[35rem] min-h-[40rem] w-full flex px-5 lg:px-20 lg:py-36 py-10">
+		<div className="bg-[url('/assets/4Asset_27foto-BIEN.jpg')] bg-center bg-cover lg:min-h-[35rem] min-h-[40rem] w-full flex px-5 lg:px-20 lg:py-36 py-10">
 			<div className="flex flex-col lg:w-1/2 w-full gap-y-5 lg:justify-items-start lg:items-start justify-center items-center">
 				<p className="text-white text-5xl font-bold lg:w-[70%] w-full lg:text-left text-center">
 					Favorece la continuidad de tus estudios
