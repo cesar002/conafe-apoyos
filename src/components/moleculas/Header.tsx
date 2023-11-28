5598a1d212a70b3066e5d1e9e26485e02792be98
Julio César <soul.unleashed13@gmail.com>
2023-11-28 11:58:54 -0600

    Se cambio el logo de conafe

--- src/components/moleculas/Header.tsx
+++ src/components/moleculas/Header.tsx
@@ -4,15 +4,15 @@ const Header = () => {
 
 	return (
 		<div className="min-h-[15rem] w-full bg-[#9d2344] flex lg:flex-row flex-col lg:justify-start justify-center items-center gap-y-5">
-			<div className="w-1/4 flex justify-center items-center">
+			<div className="md:w-1/4 w-full flex justify-center items-center">
 				<Image
-					src="/assets/02-Asset_10logo52.svg"
+					src="/assets/fondo_conafe_nuevo.svg"
 					alt="Conafe Logo"
 					width={300}
 					height={100}
 				/>
 			</div>
-			<div className="w-3/4 flex justify-center items-center lg:px-10">
+			<div className="md:w-3/4 w-full flex justify-center items-center lg:px-10">
 				<p className="text-center lg:text-5xl text-2xl font-bold text-white">
 					Apoyo tecnólogico para Educadores Comunitarios
 				</p>
