c80b51001ccc0f0ee6731545da772e7e227e9748
Julio César <soul.unleashed13@gmail.com>
2023-12-05 18:06:35 -0600

    Quitado

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-	return [
-		{
-			source: '/',
-			destination: '/mantenimiento',
-			permanent: false,
-		},
-		{
-			source: '/registro',
-			destination: '/mantenimiento',
-			permanent: false,
-		},
-	];
-  },
 }
 
 module.exports = nextConfig
