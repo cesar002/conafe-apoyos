5bd9ca8ad8a49c15282f00bc170820fe7f56f5e7
Julio <soul.unleashed13@gmail.com>
2025-02-10 14:10:53 -0600

    Se agrego video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,10 +21,13 @@ const Home = () => {
 		<div className="min-h-screen bg-[#e4f1e7]">
 			<Header />
 			<FavoreceSeccion />
-			<div className="bg-[url('/assets/kid.jpg')] bg-center bg-no-repeat bg-cover lg:min-h-[40rem] min-h-[30rem] w-full p-10 flex justify-end">
+			<div className="bg-[url('/assets/kid.jpg')] bg-center bg-no-repeat bg-cover lg:min-h-[40rem] min-h-[30rem] w-full p-10 flex lg:flex-row flex-col justify-between">
+				<div className="xl:w-1/2 w-full h-full flex justify-center items-center min-h-[30rem]">
+					<iframe width="660" height="415" src="https://www.youtube.com/embed/_Xb3vuNDteE?si=aF4ZPy-RKfZdrSwA&autoplay=1&mute=2" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+				</div>
 				<div className="flex flex-col xl:w-1/2 w-full gap-y-5 lg:justify-items-start lg:items-start justify-center items-center">
 					<p className="text-white text-4xl lg:text-5xl font-bold lg:text-left text-center">
-						{ t('seccion_2.titulo') }
+						{ t('seccion_2.titulo') }asdasd
 					</p>
 					<p className="text-white text-3xl font-light lg:text-left text-center">
 						{ t('seccion_2.parrafo_1') }
