d86a7f53b5f53917ba015be1a7d33cf98c4cdcf5
Strappberry LLC <strappberry@MacBook-Air-de-Strappberry.local>
2025-02-10 10:01:41 -0600

    Correcciones en cambio de idioma

--- src/components/moleculas/FavoreceSeccion.tsx
+++ src/components/moleculas/FavoreceSeccion.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useTranslations } from "next-intl";
 
 const FavoreceSeccion = () => {
+	const { locale } = useRouter();
 	const t = useTranslations();
 
 	return (
 		<div className="bg-[url('/assets/4Asset_27foto-BIEN.jpg')] bg-center bg-cover lg:min-h-[35rem] min-h-[40rem] w-full flex px-5 lg:px-20 lg:py-36 py-10">
 			<div className="flex flex-col xl:w-1/2 w-full gap-y-5 lg:justify-items-start lg:items-start justify-center items-center">
-				<p className="text-white text-4xl lg:text-5xl font-bold lg:text-left text-center">
+				{ locale == 'en' &&
+				<h2 className="text-white text-4xl lg:text-5xl font-bold text-left">
+					BRIDGE <br/>
+					Bridging Digital Divides: National Initiative for Educational Technology Access in Rural Mexico
+				</h2>
+				}
+
+				<p className={`text-white ${ locale == 'en' ? 'text-3xl' : 'text-4xl lg:text-5xl' } font-bold lg:text-left text-center`}>
 					{ t('seccion_1.titulo') }
 				</p>
 				<p className="text-white text-3xl font-light lg:text-left text-center">
